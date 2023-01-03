f368d0aef0aacfb15d0e5897075c965bf5f596ba
jpromanonet <programming@jpromano.net>
2023-01-03 20:20:27 -0300

    Clase 00

---
+++ index.js
@@ -0,0 +1,11 @@
+/* Vamos a declarar variables */
+/* metodo de declaracion (let, const o var) + nombre significativo de variable = contenido de la variable */
+
+let pelicula = "Toy Story"
+let serie = "The Office"
+const serie2 = "Breaking Bad"
+
+/* Vamos a mostrar nuestras variables en consola */
+console.log(pelicula)
+console.log(serie)
+console.log(serie2)
